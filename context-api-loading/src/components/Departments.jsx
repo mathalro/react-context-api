316f1e4d17d294257f7224103d4f58e0475b77c0
Matheus de Almeida Rosa <matheusp@take.net>
2021-09-13 23:16:18 -0300

    create loading component

--- context-api-loading/src/components/Departments.jsx
+++ context-api-loading/src/components/Departments.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
  
 import { getDepartments } from '../services/api';
+import Loading from './Loading';
 
 const Departments = () => {
+    const [isLoading, setIsLoading] = useState(false);
 
     const getDepartmentsAsync = async () => {
-        const response = await getDepartments();
+        setIsLoading(true);
+        const response = await getDepartments().then(response => {
+            setIsLoading(false);
+            return response;
+        });
         console.log({ response });
     }
 
     return (
-        <button onClick={getDepartmentsAsync}>Buscar departamentos</button>
+        <>
+            <button onClick={getDepartmentsAsync}>Buscar departamentos</button>
+            <Loading loading={isLoading} message='Carregando departamentos...' />
+        </>
     );
 }
 
--- context-api-loading/src/components/Users.jsx
+++ context-api-loading/src/components/Users.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
  
 import { getUsers } from '../services/api';
+import Loading from './Loading';
 
 const Users = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+
     const getUsersAsync = async () => {
-        const response = await getUsers();
+        setIsLoading(true);
+        const response = await getUsers().then(response => {
+            setIsLoading(false);
+            return response;
+        });
         console.log({ response });
     }
 
     return (
-        <button onClick={getUsersAsync}>Buscar usuários</button>
+        <>
+            <button onClick={getUsersAsync}>Buscar usuários</button>
+            <Loading loading={isLoading} message='Carregando usuários...' />
+        </>
     );
 }
 
